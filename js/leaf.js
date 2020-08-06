5b4f46c8c4caece4173addcfcf5efca5bd962ddf
Justine <tinejus.vostpro@gmail.com>
2020-08-06 10:05:41 -0400

    Create leaf.js

---
+++ js/leaf.js
@@ -0,0 +1,7 @@
+var animIcons = lottie.loadAnimation({
+  container: document.getElementById('reveal_leaf'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '../images/data.json' // the path to the animation json
+});
