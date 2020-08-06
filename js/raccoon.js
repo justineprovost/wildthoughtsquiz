6952227e1e8f5d9c9e499c5792747174cc8a67e0
Justine <tinejus.vostpro@gmail.com>
2020-08-06 10:04:22 -0400

    Create raccoon.js

---
+++ js/raccoon.js
@@ -0,0 +1,7 @@
+var animIcons = lottie.loadAnimation({
+  container: document.getElementById('leaf_bg'),
+  renderer: 'svg',
+  loop: false,
+  autoplay: true,
+  path: '../images/raccoon/data.json' // the path to the animation json
+});
